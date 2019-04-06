13e8f9ae40228e3a106fd42990e5a0f0ca9fbb20
First Dragon <firstvdragon@gmail.com>
2019-04-06 17:58:44 +0300

    Innitial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+
+  $("#screen_prev_img").click(function() {
+    $("#articles").css({opacity: 0});
+
+  });
+
+});
+$(function() {
+
+  $("#screen_prev_img").dblclick(function() {
+    $("#articles").css({opacity: 1});
+
+  });
+
+});
